2aa5bf579a6f0f7ecbc519b4e23c2ef226c2d657
brunoalbim <contato@bruno.art.br>
2023-06-14 15:11:05 -0300

    Update rd-station-crm-plugin-wp.php

--- rd-station-crm-plugin-wp.php
+++ rd-station-crm-plugin-wp.php
@@ -1,8 +1,7 @@
 <?php
 /*
   Plugin Name: RD Station CRM
-  Plugin URI: #
-	description: Plugin de integração com a plataforam RD STATION CRM.
+  Description: Plugin de integração com a plataforam RD STATION CRM.
   Version: 1.0.0
   Author: Bruno albim
   Author URI: bruno.art.br
@@ -12,7 +11,7 @@
 
 // OBTER DADOS DO PLUGIN
 require_once(ABSPATH.'wp-admin/includes/plugin.php');
-$hotwpbr_plugin_data = get_plugin_data( __FILE__ );
+$rdcrmplugin_plugin_data = get_plugin_data( __FILE__ );
 
 
 // FUNÇÕES GERAIS E SHORTCODE
